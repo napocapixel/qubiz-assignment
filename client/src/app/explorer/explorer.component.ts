7eab032978043f03a86389a6fb7ed0d4057297d1
Bogdan Neamtu <bogdan.neamtu@fabritglobal.com>
2025-10-04 14:18:07 +0300

    Created files component

--- client/src/app/explorer/explorer.component.ts
+++ client/src/app/explorer/explorer.component.ts
@@ -2,12 +2,11 @@ import { Component, computed, inject, signal } from "@angular/core";
 import { FilesService } from "../shared/data-access/files.service";
 import { ExplorerFormComponent } from "./ui/explorer-form/explorer-form.component";
 import { FileInfo } from "../shared/models/file-info.model";
-import { catchError, filter, Observable, of, switchMap, tap } from "rxjs";
+import { catchError, debounceTime, filter, Observable, of, switchMap, tap } from "rxjs";
 import { AsyncPipe } from "@angular/common";
-import { FileTypePipe } from "../shared/pipes/file-type.pipe";
-import { FileType } from "../shared/enums/file-type.enum";
 import { toObservable } from "@angular/core/rxjs-interop";
 import { BreadcrumbComponent } from "./ui/breadcrumb/breadcrumb.component";
+import { FilesComponent } from "./ui/files/files.component";
 
 @Component({
   selector: 'app-explorer',
@@ -17,28 +16,27 @@ import { BreadcrumbComponent } from "./ui/breadcrumb/breadcrumb.component";
     ExplorerFormComponent,
     BreadcrumbComponent,
     AsyncPipe,
-    FileTypePipe
+    FilesComponent
   ]
 })
 export default class ExplorerComponent {
-
     private filesService = inject(FilesService);
 
-    searchText = signal<string>('');
+    searchText = signal<string>('C:\\Users');
+    errorMessage = signal<string>('');
     breadcrumbs = computed(() => this.searchText().length > 0 ? this.searchText().split('\\').map((name, index, array) => ({
         name,
         path: array.slice(0, index + 1).join('\\')
     })) : []);
-    errorMessage: string = '';
-    fileTypeEnum = FileType;
-
+    
     files$: Observable<FileInfo[]> = toObservable(this.searchText).pipe(
-        tap((_) => this.errorMessage = ''),
+        tap((_) => this.errorMessage.set('')),
+        debounceTime(200),
         filter((searchText: string) => searchText.length > 0),
         switchMap((searchText: string) => 
             this.filesService.getFiles(searchText).pipe(
                 catchError((err) => {
-                    this.errorMessage = err.error?.error || err.message;
+                    this.errorMessage.set(err.error?.error || err.message);
                     return of([]);
                 })
             )
---
+++ client/src/app/explorer/ui/files/files.component.ts
@@ -0,0 +1,20 @@
+import { Component, input, output } from "@angular/core";
+import { FileInfo } from "../../../shared/models/file-info.model";
+import { FileTypePipe } from "../../../shared/pipes/file-type.pipe";
+import { DatePipe } from "@angular/common";
+import { FileType } from "../../../shared/enums/file-type.enum";
+
+@Component({
+    selector: 'app-files',
+    templateUrl: './files.component.html',
+    styleUrls: ['./files.component.scss'],
+    imports: [
+        FileTypePipe,
+        DatePipe
+    ]
+})
+export class FilesComponent {
+    files = input<FileInfo[]>([]);
+    pathSelected = output<string>();
+    fileTypeEnum = FileType;
+}
